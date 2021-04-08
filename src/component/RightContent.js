9c8645959a76f8cb3470c2c76dafb1cc2b1f6950
tariqsyed1 <trexrh@gmail.com>
2021-04-08 16:51:54 -0400

    first commit

--- src/component/RightContent.js
+++ src/component/RightContent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Project from './Project'
+import Experience from './Experience'
 import './panel.css'
 import wave from '../assets/img/wave.gif'
 
@@ -14,15 +15,14 @@ class RightContent extends React.Component {
     return (
         <div className="RightContent" data-aos='fade-up'>
             <div className='About'>
-                <h1>ABOUT</h1>
-                <p>Hey there! I'm Tariq <img id="wave" src={wave} alt="Wave Emoji" /></p> 
-                <p>I'm a <span id="bold-text">third year computer science </span> student at <span id="bold-text">York University</span>.
-                </p>
-                <p>Currently, I'm doing an internship at <span id="bold-text">CaseWare</span> as a <span id="bold-text">Software Developer in Test</span>. This summer, I'm joining <span id="bold-text">PRN Digital</span> as a <span id="bold-text">Software Engineer Intern</span>.</p>
-                <p>I'm looking for a <span id="bold-text">Fall 2021</span> internship! You can find my resume <a href="">here</a></p>
+                <p className="hello-text">Hey there! I'm <span id="bold-text">Tariq</span> <img id="wave" src={wave} alt="Wave Emoji" /></p>
+                <h2>EXPERIENCE</h2>
+                <Experience name="PRN Digital" position="Software Engineer Intern" date="Summer 2021" />
+                <Experience name="CaseWare" position="Test Developer Intern" date="Winter 2021" />
+                <Experience name="York University" position="BSc Computer Science" date="Summer 2023" />
             </div>
             <div className="Projects">
-              <h1>PROJECTS</h1>
+              <h2>PROJECTS</h2>
                 <Project name="Calendar Menubar" language="Node.js" link="" description="Displays a dynamically rendered calendar in the menu bar for quick access. Lightweight and cross-platform compatible."/>
                 <Project name="Project Name" language="Python" link="" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doloore magna aliqua."/>
             </div>
